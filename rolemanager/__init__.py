1d2138a291ff3672763366bcc8c109ccb6858116
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-04-28 03:53:25 +0200

    Update __init__.py

--- rolemanager/__init__.py
+++ rolemanager/__init__.py
@@ -1,6 +1,5 @@
-from .rolemanager import RoleManager  # No change, this should be correct
+from .rolemanager import RoleManager
 
 async def setup(bot):
     cog = RoleManager(bot)
     await bot.add_cog(cog)
-    await cog.sync_slash_commands()
