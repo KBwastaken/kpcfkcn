f30802953c9a648a266bf9bc950daaf758b0e2d1
KBwastaken <141185091+KBwastaken@users.noreply.github.com>
2025-04-30 05:58:35 +0200

    Update appeal.py

--- appeal/appeal.py
+++ appeal/appeal.py
@@ -9,6 +9,16 @@ class Appeal(commands.Cog):
     @commands.command()
     async def appeal(self, ctx):
         """Sends the appeal form link."""
-        await ctx.send("oopsie whoopsie got yourself or a friend of yours banned?)  
-Fix it here\n\n"
-"https://forms.gle/gR6f9iaaprASRgyP9"),
+        await ctx.send(
+            "Oopsie whoopsie, got yourself or a friend of yours banned?\n\n"
+            "Fix it here:\n"
+            "https://forms.gle/gR6f9iaaprASRgyP9"
+        )
+    @commands.command()
+    async def appeals(self, ctx):
+        """Sends the appeal form link."""
+        await ctx.send(
+            "Oopsie whoopsie, got yourself or a friend of yours banned?\n\n"
+            "Fix it here:\n"
+            "https://forms.gle/gR6f9iaaprASRgyP9"
+        )
